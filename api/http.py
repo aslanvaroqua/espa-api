a1140c20ecad0f3f2b245f9dee285b16a1e48e22
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-03-23 18:12:56 -0500

    ENH: use hug for api

--- api/http.py
+++ api/http.py
@@ -1,8 +1,8 @@
-import os
+"""First API, local access only"""
+import hug
 
-print('hello world')
-
-def application(*args, **kwargs):
-    print('args: {}'.format(args))
-    print('kwargs: {}'.format(kwargs))
-    return b'Hi'
+@hug.local()
+def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
+    """Says happy birthday to a user"""
+    return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
+            'took': float(hug_timer)}
