51d68453a399aca5caca5903af49070d60b9c004
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-03-23 22:44:03 -0500

    TST: bad ers cassette location

--- test/__init__.py
+++ test/__init__.py
@@ -5,6 +5,7 @@ cassettes = {
     'ers':  'test/resources/ers-cassette.yaml',
     'm2m':  'test/resources/m2m-cassette.yaml',
     'tram': 'test/resources/tram-cassette.yaml',
+    'ers.bad':  'test/resources/ers-cassette.bad.yaml',
 }
 
 from hypothesis import given, note, strategies as st
