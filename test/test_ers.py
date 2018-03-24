0bc3e9403f8ce1bb9efe6bf95f72fb698b3ee9eb
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-03-23 22:40:47 -0500

    TST: move ers errors into .bad file

--- test/test_ers.py
+++ test/test_ers.py
@@ -18,7 +18,7 @@ def test_login():
 @test.vcr.use_cassette(test.cassettes['ers'])
 def test_bad_login():
     with pytest.raises(ers.ErsInvalidLogin):
-        token = ers.login('json', 'json', secret='46b5bdef021a6d1c', url='http://ers/jsonapi')
+        token = ers.login('json', 'json', secret='46b5bdef021a6d1c', url='http://ers/noauth')
 
 def test_bad_url():
     with pytest.raises(ers.ErsUnavailable) as exc:
@@ -29,3 +29,13 @@ def test_roles():
     user = ers.roles('fb0fc56dd0692391', url='http://ers/api', verify=False)
     assert isinstance(user, tuple)
     assert len(user) == 4
+
+@test.vcr.use_cassette(test.cassettes['ers.bad'])
+def test_bad_login_response_format():
+    with pytest.raises(ers.ErsInvalidLogin):
+        token = ers.login('json', 'json', secret='46b5bdef021a6d1c', url='http://ers/xmlapi')
+
+@test.vcr.use_cassette(test.cassettes['ers.bad'])
+def test_bad_login_response_data():
+    with pytest.raises(ers.ErsInvalidResponse):
+        token = ers.login('json2', 'json2', secret='46b5bdef021a6d1c', url='http://ers/json2api')
