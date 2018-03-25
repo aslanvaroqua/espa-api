a3360fdcff6943d524c926345e0f3a8441f73eea
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-03-25 14:19:37 -0500

    TST: mock environ conflicts in travis

--- test/test_cfg.py
+++ test/test_cfg.py
@@ -1,4 +1,3 @@
-import os
 import mock
 
 import pytest
@@ -6,16 +5,16 @@ import pytest
 from api.config import cfg
 
 
-@mock.patch.dict(os.environ, {'ESPA_API_DB_USERNAME': 'george'})
+@mock.patch.dict('os.environ', {'ESPA_API_DB_USERNAME': 'george'})
 def test_environ():
     assert cfg.get('db')['USERNAME'] == 'george'
     assert 'username' in cfg.get('db', lower=True)
 
-@mock.patch.dict(os.environ, {'ESPA_API_CONFIG_PATH': '/path/to/nowhere'})
+@mock.patch.dict('os.environ', {'ESPA_API_CONFIG_PATH': '/path/to/nowhere'})
 def test_config_not_found():
     assert {} == cfg.get('db')
     assert 'path' in cfg.get('config', lower=True)
 
-@mock.patch.dict(os.environ, {'ESPA_API_CONFIG_PATH': './run/config.ini'})
+@mock.patch.dict('os.environ', {'ESPA_API_CONFIG_PATH': './run/config.ini'})
 def test_local_config():
     assert 'key' in cfg.get('config', lower=True)
