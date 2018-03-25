8b7879ed99bcef9054b6d2e7278d7d252b5233e1
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-03-25 12:24:52 -0500

    DOC: docstrings api.config

--- api/config/cfg.py
+++ api/config/cfg.py
@@ -4,19 +4,45 @@ import os
 import configparser
 
 
-def from_env(section):
-    prefix = 'ESPA_API_{}_'.format(section.upper())
+def from_env(section, prefix='ESPA_API'):
+    """ Read configuration values from environment variables
+
+    Args:
+        section (str): the section of config to read
+        prefix (str): prefix to section to search for
+
+    Returns:
+        dict: current config
+    """
+    prefix += '_{}_'.format(section.upper())
     return {k.split(prefix)[1]:v
             for k, v in os.environ.items() if k.startswith(prefix)}
 
 
 def cfgfile(config_file=None):
+    """ Path to configuration file; if not provided, read from env or default
+
+    Args:
+        config_file (str): path to config to override environment variable
+
+    Returns:
+        str: path to configuration
+    """
     return config_file or os.getenv('ESPA_API_CONFIG_PATH',
                                     os.path.join(os.path.expanduser('~/.usgs'),
                                                  '.espa_api.ini'))
 
 
 def from_file(section, config_file=None):
+    """ Read all configuration values from a section
+
+    Args:
+        section (str): the section to read
+        config_file (str): path to a configuration file
+
+    Returns:
+        dict: current configuration
+    """
     if not os.path.exists(cfgfile(config_file)):
         return {}
     config = configparser.ConfigParser()
@@ -26,5 +52,15 @@ def from_file(section, config_file=None):
 
 
 def get(section, config_file=None, lower=False):
+    """ Read config, first from file, override by env vars
+
+    Args:
+        section (str): section of config to read
+        config_file (str): path to config file
+        lower (bool): convert all keys to lowercase
+
+    Returns:
+        dict: current config
+    """
     return {k.lower() if lower else k: v for k, v in
             dict(from_file(section, config_file), **from_env(section)).items()}
