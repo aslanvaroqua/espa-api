f59a3e025f7636167bf54e7eeb4f394dec7db886
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-03-25 10:22:28 -0500

    TST: mock environ changes

--- test/test_cfg.py
+++ test/test_cfg.py
@@ -1,20 +1,21 @@
 import os
+import mock
 
 import pytest
 
 from api.config import cfg
 
 
+@mock.patch.dict(os.environ, {'ESPA_API_DB_USERNAME': 'george'})
 def test_environ():
-    os.environ['ESPA_API_DB_USERNAME'] = 'george'
     assert cfg.get('db')['USERNAME'] == 'george'
     assert 'username' in cfg.get('db', lower=True)
 
+@mock.patch.dict(os.environ, {'ESPA_API_CONFIG_PATH': '/path/to/nowhere'})
 def test_config_not_found():
-    os.environ['ESPA_API_CONFIG_PATH'] = '/path/to/nowhere'
     assert {} == cfg.get('db')
     assert 'path' in cfg.get('config', lower=True)
 
+@mock.patch.dict(os.environ, {'ESPA_API_CONFIG_PATH': './run/config.ini'})
 def test_local_config():
-    os.environ['ESPA_API_CONFIG_PATH'] = './run/config.ini'
     assert 'key' in cfg.get('config', lower=True)
