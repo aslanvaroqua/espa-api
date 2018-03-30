1ef77efcf38766e073d55a51781412307c4bc284
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-03-30 18:25:40 -0500

    TST: user authentication

--- test/test_http.py
+++ test/test_http.py
@@ -1,6 +1,9 @@
-import pytest
+import mock
+from base64 import b64encode
+
 import hug
-from falcon import HTTP_400, HTTP_404, HTTP_200
+import pytest
+from falcon import HTTP_400, HTTP_401, HTTP_404, HTTP_200
 
 from api.transport import http
 
@@ -14,7 +17,20 @@ def test_versions():
     assert response.status == HTTP_404
     assert response.data == {'errors': {'Not found on the server': '/api/v2a'}}
 
-def test_v2_user():
+def test_v2_user_not_auth():
     response = hug.test.get(http, '/api/v2a/user')
+    assert response.status == HTTP_401
+    assert response.data == {'errors': {'Authentication Required': 'Please provide valid Basic HTTP Authentication credentials'}}
+
+@pytest.fixture
+def auth_header():
+    return {
+        'Authorization': b'Basic ' + b64encode('{0}:{1}'.format('Tim Burton', 'Wrong password').encode('utf8'))
+    }
+
+@mock.patch('api.auth.ers.login', lambda x,y: x)
+@mock.patch('api.auth.ers.roles', lambda x: x)
+def test_v2_user(auth_header):
+    response = hug.test.get(http, '/api/v2a/user', headers=auth_header)
     assert response.status == HTTP_200
-    assert response.data == 'hello /user!'
+    assert response.data == "hello Tim Burton!"
